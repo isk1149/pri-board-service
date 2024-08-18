d3301533d94d49ce1dea834e4a3ec145fd212787
Inseong Kim <isk1149@naver.com>
2024-08-18 14:42:55 +0900

    init

---
+++ src/test/java/com/joongnior/pri_board_service/PriBoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joongnior.pri_board_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriBoardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
